a2595504d447640fc1099083ab451c65ce4a288e
bensworth <ben.s.southworth@gmail.com>
2021-04-20 11:11:54 -0600

    Modified GPUs, path to run

--- train.py
+++ train.py
@@ -28,7 +28,8 @@ def main(args):
         trainer = Trainer(
             fast_dev_run=bool(args.dev),
             logger=logger if not bool(args.dev + args.test_phase) else None,
-            gpus=-1,
+            # gpus=-1,
+            gpus=None,
             deterministic=True,
             weights_summary=None,
             log_every_n_steps=1,
@@ -57,13 +58,11 @@ if __name__ == "__main__":
     parser = ArgumentParser()
 
     # PROGRAM level args
-    parser.add_argument("--data_dir", type=str, default="/data/huy/cifar10")
+    parser.add_argument("--data_dir", type=str, default="/Users/southworth/Software/Python/Pycharm/torch/data/cifar10/")
     parser.add_argument("--download_weights", type=int, default=0, choices=[0, 1])
     parser.add_argument("--test_phase", type=int, default=0, choices=[0, 1])
     parser.add_argument("--dev", type=int, default=0, choices=[0, 1])
-    parser.add_argument(
-        "--logger", type=str, default="tensorboard", choices=["tensorboard", "wandb"]
-    )
+    parser.add_argument("--logger", type=str, default="tensorboard", choices=["tensorboard", "wandb"])
 
     # TRAINER args
     parser.add_argument("--classifier", type=str, default="resnet18")
